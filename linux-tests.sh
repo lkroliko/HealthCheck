e48a729f84d6814b0d30252dbf9f8312aa1bf488
lkroliko <lkroliko@gmail.com>
2025-01-29 20:30:13 +0100

    fix

--- linux-tests.sh
+++ linux-tests.sh
@@ -1,12 +1,5 @@
-#dotnet nuget config set repositoryPath /tmp --configfile nuget.config
-
 dotnet clean
-dotnet build
-
+dotnet build dotnet build ./tests/HealthCheck.EndToEndTests
 cp ./nugets/* ./tests/TestContainer.Net8
 cp ./nugets/* ./tests/TestContainer.Net9
-#docker build -t localhost/test-container:net8 ./tests/TestContainer.Net8
-#docker build -t localhost/test-container:net9 ./tests/TestContainer.Net9
 dotnet test
-
-#nuget config -set repositoryPath= -configfile nuget.config
